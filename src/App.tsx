677520c327ec2944870472b4bb1604d3119088d1
Alex Turpin <alex.turpin@gmail.com>
2022-02-06 20:35:44 +0800

    Google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,26 @@ import data from "./data.json"
 import Crossword from '@jaredreisinger/react-crossword';
 
 function App() {
+  const completed = () => {
+    // TODO
+  }
+
   return (
     <div className="crossword-container">
-     <Crossword data={data} theme={{
+     <Crossword
+      data={data}
+      theme={{
        columnBreakpoint: "10000px",
        highlightBackground: "#a7d8fe",
        focusBackground: "#ffd902"
-     }} />
+      }}
+      onCorrect={(direction, number, answer) => gtag("event", "correct", { answer })}
+      onCrosswordCorrect={() => {
+        completed()
+        gtag("event", "completed")
+      }}
+      onLoadedCorrect={completed}
+    />
     </div>
   )
 }
